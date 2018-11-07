a310527ca25ae65224fc3afbabe9d710a2e3d438
Samar Sajnani <ssajnani@uwo.ca>
2018-11-07 15:57:27 -0500

    changes to Interface

--- LifeVectorServer/BluetoothInterface.cpp
+++ LifeVectorServer/BluetoothInterface.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 
 BluetoothInterface::BluetoothInterface(){
-    uniqueID = getUniqueID(FILENAME);
     listen();
 
 }
@@ -11,7 +10,7 @@ BluetoothInterface::BluetoothInterface(){
 int BluetoothInterface::listen(){
     // allocate socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-    // bind socket to port 1 of the first available 
+    // bind socket to port 1 of the first available
     // local bluetooth adapter
     loc_addr.rc_family = AF_BLUETOOTH;
     str2ba( address, &loc_addr.rc_bdaddr);
@@ -25,60 +24,64 @@ int BluetoothInterface::listen(){
 string BluetoothInterface::connectAndRead(){
     // accept one connection
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
-    
+
     ba2str( &rem_addr.rc_bdaddr, buf );
     fprintf(stderr, "accepted connection from %s\n", buf);
     memset(buf, 0, sizeof(buf));
-    
+
     // read data from the client
     bytes_read = read(client, buf, sizeof(buf));
     if( bytes_read > 0 ) {
         printf("received [%s]\n", buf);
     }
+
+    string bufferVal(buf)
+    return bufferVal;
+}
+
+bool BluetoothInterface::write(string phrase){
+    write(client, phrase, )
+}
+
+int BluetoothInterface::closeClientConnection(){
     close(client);
-    string s(buf)
-    return s;
+    return 0;
 }
 
 int BluetoothInterface::stopListening(){
     // close connection
     close(s);
     return 0;
-      
+
 }
 
 /* Open the address file corresponding the network interface and extract the MAC Address*/
-int NetworkInterface::identifyMACAddress (string ifaceName){
-    string ifaceFileLine;
+bool NetworkInterface::confirmUniqueID (string testID){
     ifstream file;
-    file.open(FILENAME + ifaceName + "/address");
-    while (getline(file, ifaceFileLine)){
-        MACAddress += ifaceFileLine;
+    string devID;
+    file.open("/home/deviceID");
+    while (getline(file, devID)){
+        uniqueId = devID;
+    }
+    if (!uniqueId.compare(testID)){
+        return true
+    } else {
+        return false
     }
 }
 
 
-ostream& operator<<(ostream& os, const NetworkInterface& networkInterface){
-    os << "Network Interface Name: ";
-    os << networkInterface.getInterfaceName();
-    os << "\n"; 
-    os << "Interface MAC Address: ";
-    os << networkInterface.getMACAddress();
-    os << "\n";
-    return os;
-}
-
 BluetoothInterface();
-	    std::string read() const;
-	    	    std::string write() const;
-		    	    std::string listen() const;
-			    	    std::string stopListening() const
-				    	    std::string getInterfaceName() const;
-					    	    bool confirmUniqueID(std::string uID) const;
-						    	private:
-							            std::string uniqueId;
-								    	    const std::string FILENAME = std::string("/home/uniqueID");
-									    	    int getUniqueID ();
+std::string read() const;
+std::string write() const;
+std::string listen() const;
+std::string stopListening() const
+std::string getInterfaceName() const;
+bool confirmUniqueID(std::string uID) const;
+private:
+std::string uniqueId;
+const std::string FILENAME = std::string("/home/uniqueID");
+int getUniqueID ();
 
 
 #include <stdio.h>
