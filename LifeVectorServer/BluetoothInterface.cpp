ed46c8ab0d5e014ac78b8893d1066a83b80fa0e9
Samar Sajnani <ssajnani@uwo.ca>
2018-11-07 13:38:22 -0500

    adding frontend

--- LifeVectorServer/BluetoothInterface.cpp
+++ LifeVectorServer/BluetoothInterface.cpp
@@ -1,17 +1,50 @@
-#include "NetworkInterface.h"
+#include "BluetoothInterface.h"
 using namespace std;
 
-/*Constructor requires a file name corresponding to it's interface name*/
-NetworkInterface::NetworkInterface(string ifaceName){
-    identifyMACAddress(ifaceName);
-    interfaceName = ifaceName;
+
+BluetoothInterface::BluetoothInterface(){
+    uniqueID = getUniqueID(FILENAME);
+    listen();
+
 }
 
-string NetworkInterface::getMACAddress() const{
-    return MACAddress;
+int BluetoothInterface::listen(){
+    // allocate socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    // bind socket to port 1 of the first available 
+    // local bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    str2ba( address, &loc_addr.rc_bdaddr);
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    // put socket into listening mode
+    listen(s, 1);
+    return s;
 }
-string NetworkInterface::getInterfaceName() const{
-    return interfaceName;
+
+string BluetoothInterface::connectAndRead(){
+    // accept one connection
+    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+    
+    ba2str( &rem_addr.rc_bdaddr, buf );
+    fprintf(stderr, "accepted connection from %s\n", buf);
+    memset(buf, 0, sizeof(buf));
+    
+    // read data from the client
+    bytes_read = read(client, buf, sizeof(buf));
+    if( bytes_read > 0 ) {
+        printf("received [%s]\n", buf);
+    }
+    close(client);
+    string s(buf)
+    return s;
+}
+
+int BluetoothInterface::stopListening(){
+    // close connection
+    close(s);
+    return 0;
+      
 }
 
 /* Open the address file corresponding the network interface and extract the MAC Address*/
@@ -35,3 +68,25 @@ ostream& operator<<(ostream& os, const NetworkInterface& networkInterface){
     return os;
 }
 
+BluetoothInterface();
+	    std::string read() const;
+	    	    std::string write() const;
+		    	    std::string listen() const;
+			    	    std::string stopListening() const
+				    	    std::string getInterfaceName() const;
+					    	    bool confirmUniqueID(std::string uID) const;
+						    	private:
+							            std::string uniqueId;
+								    	    const std::string FILENAME = std::string("/home/uniqueID");
+									    	    int getUniqueID ();
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+int main(int argc, char **argv)
+{
+}
--- LifeVectorServer/BluetoothInterface.h
+++ LifeVectorServer/BluetoothInterface.h
@@ -5,24 +5,36 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#ifndef NetworkInterface_H
-#define NetworkInterface_H
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+#ifndef BluetoothInterface_H
+#define BluetoothInterface_H
 
 /* This class was developed to gather the individual network interface based
  * on the file name within the network interface folder /sys/class/net*/
 
-class NetworkInterface {
+class BluetoothInterface {
     public:
-        NetworkInterface(std::string ifaceName);
-	    std::string getMACAddress() const;
+        BluetoothInterface();
+	    std::string read() const;
+	    std::string write() const;
+	    std::string listen() const;
+	    std::string stopListening() const
 	    std::string getInterfaceName() const;
-	    friend std::ostream& operator<<(std::ostream& os, const NetworkInterface& networkInterface);
+	    bool confirmUniqueID(std::string uID) const;
 	private:
-		std::string interfaceName, MACAddress;
-		const std::string FILENAME = std::string("/sys/class/net/");
-		int identifyMACAddress (std::string ifaceName);
+            struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+	    char buf[1024] = { 0 };
+	    int s, client, bytes_read;
+            socklen_t opt = sizeof(rem_addr);
+	    char address[18] = "XX:XX:XX:XX:XX:XX";
+            std::string uniqueId;
+	    const std::string FILENAME = std::string("/home/uniqueID");
+	    int getUniqueID ();
 };
 
 
-std::ostream& operator<<(std::ostream& os, const NetworkInterface& networkInterface);
 #endif
