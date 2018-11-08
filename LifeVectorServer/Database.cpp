4a5f0e1410a889cc57468d50967e3a33737a826d
Rukun Wang <rwang264@uwo.ca>
2018-11-07 21:05:06 -0500

    general formatting and error message clarification

--- LifeVectorServer/Database.cpp
+++ LifeVectorServer/Database.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
 #include "Database.h"
 #include <typeinfo>
 #include <sstream>
@@ -7,40 +7,40 @@ using namespace std;
 
 Database::Database()
 {
-	connection = mysql_init(NULL); 
+	connection = mysql_init(NULL);
 	if (connection == NULL)
 	{
-		cout << "Error:" << mysql_error(connection);
+		cout << "Object Initialization Error:" << mysql_error(connection);
 		exit(1);
 	}
 }
 
-Database :: ~Database()
+Database::~Database()
 {
-	if (connection != NULL) 
+	if (connection != NULL)
 	{
 		mysql_close(connection);
 	}
 }
 
-bool Database :: initDB(string host, string user, string pwd, string db_name)
+bool Database::initDB(string host, string user, string pwd, string db_name)
 {
 	// Build a connection with database
 	connection = mysql_real_connect(connection, host.c_str(),
-		user.c_str(), pwd.c_str(), db_name.c_str(), 0, NULL, 0);
+									user.c_str(), pwd.c_str(), db_name.c_str(), 0, NULL, 0);
 	if (connection == NULL)
 	{
-		cout << "Error:" << mysql_error(connection) << endl;
+		cout << "Connection Initialization Error:" << mysql_error(connection) << endl;
 		exit(1);
 	}
 	return true;
 }
 
-bool Database :: exeSQL(string sql)
+bool Database::exeSQL(string sql)
 {
 	if (mysql_query(connection, sql.c_str()))
 	{
-		cout << "Query Error:" << mysql_error(connection) << endl;
+		cout << "Query Execution Error:" << mysql_error(connection) << endl;
 		exit(1);
 	}
 	else
@@ -66,34 +66,34 @@ bool Database :: exeSQL(string sql)
 	return true;
 }
 
-std::string Database :: getSQLResult(string sql)
+std::string Database::getSQLResult(string sql)
 {
-    stringstream ss;
-    
-    if (mysql_query(connection, sql.c_str()))
-    {
-        ss << "Query Error:" << mysql_error(connection) << endl;
-        return ss.str();
-    }
-    else
-    {
-        result = mysql_use_result(connection); // Get Result
-        // mysql_field_count() return number of rows
-        for (int i = 0; i < mysql_field_count(connection); ++i)
-        {
-            // Get next row
-            row = mysql_fetch_row(result);
-            if (row <= 0)
-                break;
-            // mysql_num_fields return number of columns
-            for (int j = 0; j < mysql_num_fields(result); ++j)
-            {
-                ss << row[j] << " ";
-            }
-            ss << endl;
-        }
-        // Free memory
-        mysql_free_result(result);
-    }
-    return ss.str();
+	stringstream ss;
+
+	if (mysql_query(connection, sql.c_str()))
+	{
+		ss << "Reseult Query Error:" << mysql_error(connection) << endl;
+		return ss.str();
+	}
+	else
+	{
+		result = mysql_use_result(connection); // Get Result
+		// mysql_field_count() return number of rows
+		for (int i = 0; i < mysql_field_count(connection); ++i)
+		{
+			// Get next row
+			row = mysql_fetch_row(result);
+			if (row <= 0)
+				break;
+			// mysql_num_fields return number of columns
+			for (int j = 0; j < mysql_num_fields(result); ++j)
+			{
+				ss << row[j] << " ";
+			}
+			ss << endl;
+		}
+		// Free memory
+		mysql_free_result(result);
+	}
+	return ss.str();
 }
