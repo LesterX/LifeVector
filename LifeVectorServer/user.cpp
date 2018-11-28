9db3feabd049ab2be6ce24a423eab645b5740ee6
mario <jferna29@uwo.ca>
2018-11-28 12:47:19 -0500

    user doxygen

--- LifeVectorServer/user.cpp
+++ LifeVectorServer/user.cpp
@@ -1,14 +1,14 @@
-/**
- * cpp file for user class which contains getters and setters
+/** 
+ * @brief creates a user or sets values for existing users. Has getters and setters to accomplish this
+ * @param username, deviceID, hash, salt, syncTime
  */
-
 #include "user.h"
 
 using namespace std;
 
-// Constructors
-
-// New user - no report or times
+/** constructor for new user so no report or report time
+ * @param username, deviceID, hash, salt, syncTime
+ */
 User::User(std::string username, std::string deviceID, std::string hash, std::string salt)
 {
     self_username = username;
@@ -20,7 +20,10 @@ User::User(std::string username, std::string deviceID, std::string hash, std::st
     self_report = NULL;
 }
 
-// Full user initialization
+/** 
+ * @brief full constructor
+ * @param username, deviceID, hash, salt, report, syncTime, reportTime
+ */
 User::User(std::string username, std::string deviceID, std::string hash, std::string salt, json report, int syncTime, int reportTime)
 {
     self_username = username;
@@ -32,7 +35,10 @@ User::User(std::string username, std::string deviceID, std::string hash, std::st
     self_reportTime = reportTime;
 }
 
-// Barebone initialization
+/** 
+ * @brief barebones constructor
+ * @param username, deviceID
+ */
 User::User(string username, string deviceID)
 {
     self_username = username;
@@ -44,66 +50,122 @@ User::User(string username, string deviceID)
     self_reportTime = 0;
 }
 
-// Destructor
+/** 
+ * @brief destructor
+ */
 User::~User() {}
 
-// Getters:
+/** 
+ * @brief returns the name of the user
+ * @return self_username
+ */
 string User::getUsername()
 {
     return self_username;
 }
-
+/** 
+ * @brief returns the id of the device
+ * @return self_deviceID
+ */
 string User::getDeviceID()
 {
     return self_deviceID;
 }
+/** 
+ * @brief returns the hash
+ * @return self_hash
+ */
 string User::getHash()
 {
     return self_hash;
 }
+/** 
+ * @brief returns the salt
+ * @return self_salt
+ */
 string User::getSalt()
 {
     return self_salt;
 }
+/** 
+ * @brief returns the report
+ * @return self_report
+ */
 json User::getReport()
 {
     return self_report;
 }
+/** 
+ * @brief returns the sync time
+ * @return self_syncTime
+ */
 long User::getSyncTime()
 {
     return self_syncTime;
 }
+/** 
+ * @brief returns the report time
+ * @return self_reportTime
+ */
 long User::getReportTime()
 {
     return self_reportTime;
 }
 
-// Setters:
+/** 
+ * @brief sets the username
+ * @param username
+ */
 void User ::setUsername(string username)
 {
     self_username = username;
 }
+/** 
+ * @brief sets the device id
+ * @param deviceID
+ */
 void User ::setDeviceID(string deviceID)
 {
     self_deviceID = deviceID;
 }
+/** 
+ * @brief sets the hash
+ * @param hash
+ */
 void User ::setHash(string hash)
 {
     self_hash = hash;
 }
+/** 
+ * @brief sets the salt
+ * @param salt
+ */
 void User ::setSalt(string salt)
 {
     self_salt = salt;
 }
+/** 
+ * @brief sets the report
+ * @param report
+ */
 void User::setReport(json report)
 {
     self_report = report;
 }
+/** 
+ * @brief sets the sync time
+ * @param syncTime
+ */
 void User ::setSyncTime(long syncTime)
 {
     self_syncTime = syncTime;
 }
+/** 
+ * @brief sets the report time
+ * @param reportTime
+ */
 void User ::setReportTime(long reportTime)
 {
     self_reportTime = reportTime;
 }
+
