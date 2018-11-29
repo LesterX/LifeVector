bf19226363b6d8eacca74eaed7d2bc9a2f12d5ff
Yimin Xu <yxu537@uwo.ca>
2018-11-29 17:34:22 -0500

    Squasher works

--- LifeVectorServer/TestSquash.cpp
+++ LifeVectorServer/TestSquash.cpp
@@ -25,19 +25,14 @@ int main(int argc, char** argv) {
 	//Create database object and Raw data repository	
 	Database db;
 	db.initDB("localhost","server","LifeVector123","LifeVector");
-	RawDataRepository rdr;
+	RawDataRepository rdr("./SampleFiles/sample_coordinates.json","gps_coordinates_dump");
+	rdr.dumpData();
 
-	//Adding coordinates data into raw data reporsitory
-	rdr.addTestData(1543500000, 43.005874, -81.274616);
-	rdr.addTestData(1543501800, 43.008604, -81.275284);
-	rdr.addTestData(1543503600, 43.008604, -81.275284);
-	rdr.addTestData(1543505400, 43.008792, -81.276411);
-	rdr.addTestData(1543507200, 43.009768, -81.270752);
-
+	
 	//Create squasher
 	squasher sq(rdr, &db,"Yimin","01");
 	sq.squash();
-
+	
 	return 0;
 }
 
